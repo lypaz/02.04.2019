81ddf9faf7bdefcdd3dadad9105903e98764f821
lypaz <47334541+lypaz@users.noreply.github.com>
2019-04-02 13:33:09 +0300

    Create AlexMeyer.js

---
+++ AlexMeyer.js
@@ -0,0 +1,64 @@
+                      Chaptor A
+
+                    //First task
+
+var a = parseInt(prompt("Enter first number", 0))
+var b = parseInt(prompt("Enter second number", 0))
+if (a > b)
+  {
+    console.log(a)
+  }
+else if (b > a)
+  {
+    console.log(b)
+  }
+else
+  {
+    console.log ("the same")
+  }
+  
+  
+  
+                     //Second task
+  
+var a = parseInt( prompt("Enter first number", 0) )
+var b = parseInt( prompt("Enter second number", 0) )
+if (b != 0){
+  console.log (a/b)
+  console.log (a % b)
+}
+else {
+  console.log ("can't divide 0")
+}
+
+                      //Third task
+
+var x = parseInt(prompt("Enter number", 0))
+var sum = 0
+while (x >=0 )
+  {
+    sum = sum + x
+    x = parseInt(prompt("Enter number"), 0)
+  }
+ console.log(sum)
+ 
+                     // Fourth task
+var myArray = []
+var x = parseInt(prompt("please enter number",0))
+var sum = 0
+var a = 0
+for(var i = 0; i < 10; i++)
+{
+    myArray.push(x)
+    x = parseInt(prompt("please enter number",0))
+  sum = sum + i
+  a++
+  }
+console.log (sum/a) 
+
+
+
+
+                          Chaptor B
+                          
+                          // First task
